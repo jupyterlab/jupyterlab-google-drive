8c5744e5dc5f011636cad9a9609337b198d2d402
ian-r-rose <ian.r.rose@gmail.com>
2016-12-19 16:41:16 -0800

    Remove redundant stuff.

--- src/plugin.ts
+++ src/plugin.ts
@@ -36,82 +36,17 @@ import {
 
 let trackerSet = new Set<[InstanceTracker<Widget>, (widget: Widget)=>IRealtimeModel, (widget: Widget)=>void]>();
 
-export
 const plugin: JupyterLabPlugin<IRealtime> = {
   id: 'jupyter.services.realtime',
-  requires: [IMainMenu],
+  requires: [],
   provides: IRealtime,
   activate: activateRealtime,
   autoStart: true
 };
 
-const cmdIds = {
-  shareRealtimeFile : 'realtime:share',
-  openRealtimeFile : 'realtime:open',
-  openChatbox : 'chatbox:create-chatbox'
-};
-
-function activateRealtime(app: JupyterLab, mainMenu : IMainMenu): IRealtime {
-
+function activateRealtime(app: JupyterLab): IRealtime {
   let realtime = new GoogleRealtime();
-
-  mainMenu.addMenu(createMenu(app), {rank: 60});
-  let commands = app.commands;
-
-  commands.addCommand(cmdIds.shareRealtimeFile, {
-    label: 'Share',
-    caption: 'Share this file',
-    execute: ()=> {
-      let [widget, model, callback] = getRealtimeModel(app);
-      if (model) {
-        realtime.shareDocument(model)
-        .then( ()=>{callback(widget);} );
-      }
-    }
-  });
-  commands.addCommand(cmdIds.openRealtimeFile, {
-    label: 'Open',
-    caption: 'Open a file that has been shared with you',
-    execute: ()=> {
-      let [widget, model, callback] = getRealtimeModel(app);
-      if(model) {
-        realtime.openSharedDocument(model)
-        .then( ()=>{callback(widget);} );
-      }
-    }
-  });
-
   return realtime;
 }
 
-
-function createMenu( app: JupyterLab ) : Menu {
-
-  let {commands, keymap} = app;
-  let menu = new Menu( {commands, keymap} )
-  menu.title.label = 'Realtime'
-
-  menu.addItem( {command: cmdIds.shareRealtimeFile});
-  menu.addItem( {command: cmdIds.openRealtimeFile});
-  menu.addItem( {command: cmdIds.openChatbox});
-
-  return menu;
-}
-
-function getRealtimeModel( app: JupyterLab): [Widget, IRealtimeModel, (widget: Widget)=>void] {
-  let model: IRealtimeModel = null;
-  let callback: (widget: Widget)=>void = null;
-  let widget = app.shell.currentWidget;
-  trackerSet.forEach( ([tracker, getModel, cb]) => {
-    if (tracker.has(widget)) {
-      model = getModel(widget);
-      callback = cb;
-    }
-  });
-  return [widget, model, callback];
-}
-
-export
-function addRealtimeTracker( tracker: InstanceTracker<Widget>, getModel : (widget: Widget)=>IRealtimeModel, callback: (widget: Widget)=>void = ()=>{} ): void {
-  trackerSet.add([tracker, getModel, callback]);
-}
+export default plugin;
