c9b14e72fdc63e04cc9d8643cd84835e64abc77b
Ian Rose <ian.r.rose@gmail.com>
2017-04-20 15:03:35 -0700

    Work on updating extension build to new npm-based setup.

--- scripts/build_extension.js
+++
@@ -1,7 +0,0 @@
-var buildExtension = require('@jupyterlab/extension-builder').buildExtension;
-
-buildExtension({
-        name: 'jupyterlab_google_drive',
-        entry: './lib/plugin.js',
-        outputDir: './jupyterlab_google_drive/static'
-});
