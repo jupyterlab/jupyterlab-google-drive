6bc4be272d3ac40d8b6c7174f40e92b43e7c5c22
Ian Rose <ian.r.rose@gmail.com>
2017-09-26 14:42:26 -0700

    Write bash script to automate access token getting.

---
+++ test/get-access-token.sh
@@ -0,0 +1,7 @@
+cd offline
+npm install
+node get-access-token.js > token.txt
+source token.txt
+cd ..
+sed -i "s/<TEST_ACCESS_TOKEN>/$ACCESS_TOKEN/" src/util.ts
+sed -i "s/<TEST_CLIENT_ID>/$CLIENT_ID/" src/util.ts
