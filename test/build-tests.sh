473af091a22f4f1f77629eef3280c05f0bd08027
Ian Rose <ian.r.rose@gmail.com>
2017-10-02 10:16:24 -0700

    Build and run authenticated tests separately, based on whether the appropriate environment variables exist.

---
+++ test/build-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# Conditionally build drive tests if we have the proper environment variables
+if [ ! -z $CLIENT_ID ] && [ ! -z $CLIENT_SECRET ] && [ ! -z $REFRESH_TOKEN ]; then
+
+  echo "Building remote Google Drive tests"
+
+  # Run the script to get the access token.
+  cd get-access-token
+  npm install
+  node get-access-token.js > token.txt
+  source token.txt && rm token.txt
+  cd ..
+
+  # Patch the access token into the appropriate file
+  sed -i "s/const ACCESS_TOKEN.*$/const ACCESS_TOKEN = '$ACCESS_TOKEN'/" src/util.ts
+  sed -i "s/const CLIENT_ID.*$/const CLIENT_ID = '$CLIENT_ID'/" src/util.ts
+
+fi
+
+tsc
+webpack --config webpack.config.js
+webpack --config webpack-auth.config.js
--- test/get-access-token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd offline
-npm install
-node get-access-token.js > token.txt
-source token.txt && rm token.txt
-cd ..
-sed -i "s/const ACCESS_TOKEN.*$/const ACCESS_TOKEN = '$ACCESS_TOKEN'/" src/util.ts
-sed -i "s/const CLIENT_ID.*$/const CLIENT_ID = '$CLIENT_ID'/" src/util.ts
