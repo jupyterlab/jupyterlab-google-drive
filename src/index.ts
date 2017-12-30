3cace6285207a0b4f5a4f62ce8421aca23175c48
Ian Rose <ian.r.rose@gmail.com>
2017-12-30 08:39:57 -0800

    Add sharing to file menu.
    Capitalization.

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,10 @@ import {
 } from '@jupyterlab/filebrowser';
 
 import {
+  IMainMenu
+} from '@jupyterlab/mainmenu';
+
+import {
   IRenderMimeRegistry
 } from '@jupyterlab/rendermime';
 
@@ -78,7 +82,7 @@ namespace CommandIDs {
  */
 const fileBrowserPlugin: JupyterLabPlugin<void> = {
   id: '@jupyterlab/google-drive:drive',
-  requires: [ICommandPalette, IDocumentManager, IFileBrowserFactory, ILayoutRestorer, ISettingRegistry],
+  requires: [ICommandPalette, IDocumentManager, IFileBrowserFactory, ILayoutRestorer, IMainMenu, ISettingRegistry],
   activate: activateFileBrowser,
   autoStart: true
 };
@@ -86,7 +90,7 @@ const fileBrowserPlugin: JupyterLabPlugin<void> = {
 /**
  * Activate the file browser.
  */
-function activateFileBrowser(app: JupyterLab, palette: ICommandPalette, manager: IDocumentManager, factory: IFileBrowserFactory, restorer: ILayoutRestorer, settingRegistry: ISettingRegistry): void {
+function activateFileBrowser(app: JupyterLab, palette: ICommandPalette, manager: IDocumentManager, factory: IFileBrowserFactory, restorer: ILayoutRestorer, mainMenu: IMainMenu, settingRegistry: ISettingRegistry): void {
   const { commands } = app;
   const id = fileBrowserPlugin.id;
 
@@ -163,10 +167,34 @@ function activateFileBrowser(app: JupyterLab, palette: ICommandPalette, manager:
         });
       }
     },
-    icon: 'jp-MaterialIcon jp-ShareIcon',
-    label: 'Share'
+    isEnabled: () => {
+      const { currentWidget } = app.shell;
+      if (!currentWidget) {
+        return false;
+      }
+      const context = manager.contextForWidget(currentWidget);
+      if (!context) {
+        return false;
+      }
+      return manager.services.contents.driveName(context.path) === drive.name;
+    },
+    label: () => {
+      const { currentWidget } = app.shell;
+      let fileType = 'File';
+      if (currentWidget) {
+        const context = manager.contextForWidget(currentWidget);
+        if (context) {
+          const fts = app.docRegistry.getFileTypesForPath(context.path);
+          if (fts.length && fts[0].displayName) {
+            fileType = fts[0].displayName;
+          }
+        }
+      }
+      return `Share ${fileType} With Google Drive…`;
+    }
   });
   palette.addItem({ command, category: 'File Operations' });
+  mainMenu.fileMenu.addGroup([{ command }], 20);
 
   return;
 }
